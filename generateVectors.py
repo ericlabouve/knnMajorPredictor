986d939db9a4f3595e99cdffbd0750d90d9f217a
Eric LaBouve <elabouve@comcast.net>
2017-11-06 17:49:38 -0800

    distance function added

--- generateVectors.py
+++ generateVectors.py
@@ -2,10 +2,6 @@ import os
 from collections import defaultdict
 
 categories = ['aerospace', 'architectural']
-
-trainVectors = []  # 2-tuple of category and term frequency dictionary vectors
-testVectors = []  # 2-tuple of category and term frequency dictionary vectors
-
 noiseWordArray = ["a", "about", "above", "all", "along", "also", "although", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "e.g.", "either", "etc", "etc.", "even", "ever", "enough", "for", "from", "further", "get", "gets", "got", "had", "have", "hardly", "has", "hasn't", "having", "he", "hence", "her", "here", "hereby", "herein", "hereof", "hereon", "hereto", "herewith", "him", "his", "how", "however", "i", "i.e.", "if", "in", "into", "it", "it's", "its", "me", "more", "most", "mr", "my", "near", "nor", "now", "no", "not", "or", "on", "of", "onto", "other", "our", "out", "over", "really", "said", "same", "she", "should", "shouldn't", "since", "so", "some", "such", "than", "that", "the", "their", "them", "then", "there", "thereby", "therefore", "therefrom", "therein", "thereof", "thereon", "thereto", "therewith", "these", "they", "this", "those", "through", "thus", "to", "too", "under", "until", "unto", "upon", "us", "very", "was", "wasn't", "we", "were", "what", "when", "where", "whereby", "wherein", "whether", "which", "while", "who", "whom", "whose", "why", "with", "without", "would", "you", "your", "yours", "yes"]
 
 
@@ -19,17 +15,17 @@ def getTfVector(path):
     return termFreq
 
 
-def generateVectors():
+def generateTrainingVectors():
+    trainVectors = []  # 2-tuple of category and term frequency dictionary vectors
     for dir, category in zip(['trainTextData/' + category + '/' for category in categories], categories):  # For all training directories
         for filename in os.listdir(dir):  # For each file in all directories
             trainVectors.append((category, getTfVector(dir + filename)))
+    return trainVectors
 
+
+def generateTestingVectors():
+    testVectors = []  # 2-tuple of category and term frequency dictionary vectors
     for dir, category in zip(['testTextData/' + category + '/' for category in categories], categories):  # For all testing directories
         for filename in os.listdir(dir):  # For each file in all directories
             testVectors.append((category, getTfVector(dir + filename)))
-
-
-if __name__ == '__main__':
-    generateVectors()
-    print(str(trainVectors))
-    print(str(testVectors))
+    return testVectors
---
+++ knn.py
@@ -0,0 +1,30 @@
+from generateVectors import generateTrainingVectors
+from generateVectors import generateTestingVectors
+
+
+def euclideanDistance(v1, v2):
+    # https://stackoverflow.com/questions/18554012/intersecting-two-dictionaries-in-python
+    keys_v1 = set(v1.keys())
+    keys_v2 = set(v2.keys())
+    intersection = keys_v1 & keys_v2  # terms that v1 and v2 share
+    keys_v1 -= intersection  # terms that are only contained in v1
+    keys_v2 -= intersection  # terms that are only contained in v2
+    sum = 0
+    for key in intersection:
+        sum += (v1[key] - v2[key]) ** 2
+    for key in keys_v1:
+        sum += v1[key] ** 2
+    for key in keys_v2:
+        sum += v2[key] ** 2
+    return sum ** 1/2
+
+
+def computeKnn(k, testVector, trainingVectors):
+    # use a priority queue to order trainingVectors based on smallest distance to cmputeKnn
+    pass
+
+
+if __name__ == '__main__':
+    trainVectors = generateTrainingVectors()
+    testVectors = generateTestingVectors()
+    print(euclideanDistance(testVectors[0][1], testVectors[1][1]))
