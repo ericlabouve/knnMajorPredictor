4ff0d22c78f1e698aa74e17f9245ee38f9968b8a
Jason Chin <jchin09@calpoly.edu>
2017-11-08 21:04:35 -0800

    implemented cosine similarity

--- knn.py
+++ knn.py
@@ -1,15 +1,40 @@
 from generateVectors import generateVectors
 from collections import defaultdict
 import operator
+import math
+
+def getIntersection(v1, v2):
+    keys_v1 = set(v1.keys())
+    keys_v2 = set(v2.keys())
+    intersection = keys_v1 & keys_v2  # terms that v1 and v2 share
+
+    return intersection
+
+def cosineSimilarity(v1, v2):
+    intersection = getIntersection(v1, v2)
+    dot_product = 0
+    v1_mag = 0
+    v2_mag = 0
+    
+    for key in intersection:
+        dot_product += v1[key] * v2[key]
+
+    for key in v1.keys():
+        v1_mag += v1[key] ** 2
+
+    for key in v2.keys():
+        v2_mag += v2[key] ** 2
+    
+    #print(dot_product / math.sqrt(v1_mag * v2_mag))
+    return dot_product / math.sqrt(v1_mag * v2_mag)
+    
 
 
 def euclideanDistance(v1, v2):
     # https://stackoverflow.com/questions/18554012/intersecting-two-dictionaries-in-python
-    keys_v1 = set(v1.keys())
-    keys_v2 = set(v2.keys())
-    intersection = keys_v1 & keys_v2  # terms that v1 and v2 share
-    keys_v1 -= intersection  # terms that are only contained in v1
-    keys_v2 -= intersection  # terms that are only contained in v2
+    intersection = getIntersection(v1, v2)
+    keys_v1 = set(v1.keys()) - intersection  # terms that are only contained in v1
+    keys_v2 = set(v2.keys()) - intersection  # terms that are only contained in v2
     sum = 0
     for key in intersection:
         sum += (v1[key] - v2[key]) ** 2
@@ -20,11 +45,11 @@ def euclideanDistance(v1, v2):
     return sum ** 1/2
 
 
-def computeKnn(k, testVector, trainingVectors):
+def computeKnn(k, testVector, trainingVectors, distanceFormula):
     # use a priority queue to order trainingVectors based on smallest distance to cmputeKnn
     distances = []
     for x in range(len(trainingVectors)):
-        dist = euclideanDistance(testVector[1], trainingVectors[x][1])
+        dist = distanceFormula(testVector[1], trainingVectors[x][1])
         distances.append((trainingVectors[x], dist))
     distances.sort(key=operator.itemgetter(1))
     neighbors = []
@@ -56,7 +81,9 @@ if __name__ == '__main__':
     classifiedVectors = defaultdict(list)
     for i in range(len(allVectors)):
         vector = allVectors.pop(0)  # Remove vector from dataset
-        neighbors = computeKnn(k, vector, allVectors)
+
+        # replace the last parameter of computeKnn with the desired distance formula
+        neighbors = computeKnn(k, vector, allVectors, cosineSimilarity)
         allVectors.append(vector)  # Add vector back into dataset
         predictedClass = getMajor(neighbors)
         classifiedVectors[predictedClass].append(vector[1])
